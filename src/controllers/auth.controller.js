ad403b4823fb9d0c7df0a0b1ee772280ba699821
filipetenedini <tenedinifilipe@gmail.com>
2023-05-22 10:40:49 -0300

    (feat) add signin endpoint

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,9 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 import authRepository from '../repositories/auth.repository.js';
+
+dotenv.config();
 
 async function create(req, res) {
   const { data } = res.locals;
@@ -19,6 +23,19 @@ async function create(req, res) {
 }
 
 async function signin(req, res) {
-  console.log(res.locals);
+  const { email, password } = res.locals.body;
+  try {
+    const user = await authRepository.findByEmail(email);
+
+    const authUser = await bcrypt.compare(password, user.password);
+    if (!authUser) res.status(401).send('Incorrect email or password');
+
+    const data = { id: user.id, name: user.name, email: user.email };
+
+    const token = jwt.sign(data, process.env.JWT_SECRET);
+    res.status(200).send({ token });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
-export default { create };
+export default { create, signin };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import authController from '../controllers/auth.controller.js';
 import validateSignUpData from '../middlewares/signup.middleware.js';
+import validateBody from '../middlewares/validateBody.middleware.js';
+import authSchema from '../schemas/auth.schema.js';
 
 const authRouter = Router();
 
 authRouter.post('/signup', validateSignUpData, authController.create);
+
+authRouter.post('/signin', validateBody(authSchema.signInSchema), authController.signin);
 
 export default authRouter;
